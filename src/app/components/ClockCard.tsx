81c295e8095ff4e8aec03707a3d54e9bc2817269
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-05-21 15:03:50 -0300

    feat: adding indication whether market is open or not

--- src/app/components/ClockCard.tsx
+++ src/app/components/ClockCard.tsx
@@ -3,14 +3,21 @@ import React from 'react';
 
 interface CityClockProps {
     cityName: string;
+    marketStatus: boolean;
     time: string;
 }
 
-export const ClockCard: React.FC<CityClockProps> = ({ cityName, time }) => {
+export const ClockCard: React.FC<CityClockProps> = ({ cityName, marketStatus, time }) => {
+    const marketStatusColor = marketStatus == true ? 'text-green-500' : 'text-red-500';
+    const marketStatusText = marketStatus ? 'Aberto' : 'Fechado';
+
     return (
-        <div className="w-64 h-32 text-white p-4 rounded-lg relative flex flex-col justify-center shadow-lg border border-gray-900">
+        <div className="w-64 h-40 text-white p-4 rounded-lg relative flex flex-col justify-center shadow-lg border border-gray-900">
             <div className="absolute top-4 left-4 text-sm font-semibold">
                 {cityName}
+            </div>
+            <div className={`absolute top-4 right-4 text-sm font-semibold ${marketStatusColor}`}>
+                {marketStatusText}
             </div>
             <hr className="w-full border-t border-gray-900" />
             <div className="text-2xl font-bold text-center mt-4">
