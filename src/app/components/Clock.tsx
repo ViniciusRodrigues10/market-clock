a314ba541b331ed6a998ea82996b0c1f48e157b4
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-05-21 12:38:28 -0300

    feat: creating clock

---
+++ src/app/components/Clock.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
+type Props = {
+    time: number
+}
+
+export const Clock = ({ time: initial }: Props) => {
+    const [time, setTime] = useState(new Date(initial))
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date())
+        }, 1000)
+
+        return () => clearInterval(timer)
+    }, [])
+    
+    return <div className="text-9xl tabular-nums">{time.toLocaleTimeString()}</div>
+}
