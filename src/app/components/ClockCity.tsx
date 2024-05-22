902a3cf4eb8a445b945aeb5501d23a49597be2e5
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-05-22 12:01:54 -0300

    feat: adding hours of the main financial market exchanges

--- src/app/components/ClockCity.tsx
+++ src/app/components/ClockCity.tsx
@@ -1,6 +1,4 @@
 'use client'
-
-import React, { useState, useRef } from 'react';
 import { ClockCard } from './ClockCard';
 
  export const ClockCity = () => {
@@ -8,28 +6,25 @@ import { ClockCard } from './ClockCard';
     return (
         <div className="mx-auto max-w-screen-xl p-16 mt-20">
             <ul className="grid md:grid-cols-4 gap-8">
-                <ClockCard
-                cityName={'Sydney'}
-                temiZone={'Australia/Sydney'}
-                marketStatus={true}
-                />
-                <ClockCard
-                cityName={'São Paulo'}
-                temiZone={'America/Sao_Paulo'}
-                marketStatus={true}
-                />
-                <ClockCard
-                cityName={'New York'}
-                temiZone={'America/New_York'}
-                marketStatus={true}
-                />
-                <ClockCard
-                cityName={'Tokyo'}
-                temiZone={'Asia/Tokyo'}
-                marketStatus={false}
-                />
-
-            </ul>
+                <ClockCard cityName={'New York'} timeZone={'America/New_York'} marketStatus={true} />
+                <ClockCard cityName={'Toronto'} timeZone={'America/Toronto'} marketStatus={true} />
+                <ClockCard cityName={'Chicago'} timeZone={'America/Chicago'} marketStatus={true} />
+                <ClockCard cityName={'São Paulo'} timeZone={'America/Sao_Paulo'} marketStatus={true} />
+                <ClockCard cityName={'Wellington'} timeZone={'Pacific/Auckland'} marketStatus={true} />
+                <ClockCard cityName={'Sydney'} timeZone={'Australia/Sydney'} marketStatus={true} />
+                <ClockCard cityName={'Tokyo'} timeZone={'Asia/Tokyo'} marketStatus={false} />
+                <ClockCard cityName={'Shanghai'} timeZone={'Asia/Shanghai'} marketStatus={true} />
+                <ClockCard cityName={'Hong Kong'} timeZone={'Asia/Hong_Kong'} marketStatus={true} />
+                <ClockCard cityName={'Singapore'} timeZone={'Asia/Singapore'} marketStatus={true} />
+                <ClockCard cityName={'Mumbai'} timeZone={'Asia/Kolkata'} marketStatus={true} />
+                <ClockCard cityName={'Dubai'} timeZone={'Asia/Dubai'} marketStatus={true} />
+                <ClockCard cityName={'Riyadh'} timeZone={'Asia/Riyadh'} marketStatus={true} />
+                <ClockCard cityName={'Johannesburg'} timeZone={'Africa/Johannesburg'} marketStatus={true} />
+                <ClockCard cityName={'Moscow'} timeZone={'Europe/Moscow'} marketStatus={true} />
+                <ClockCard cityName={'Frankfurt'} timeZone={'Europe/Berlin'} marketStatus={true} />
+                <ClockCard cityName={'Zurich'} timeZone={'Europe/Zurich'} marketStatus={true} />
+                <ClockCard cityName={'London'} timeZone={'Europe/London'} marketStatus={true} />
+                </ul>
         </div>
     );
 };
