e8f1025ddc2fb78101a0465d00c91599a05396ae
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-05-23 11:13:19 -0300

    refactor: refactoring code that changes the clock font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
 import { Clock } from './components/Clock';
 import { Calendar } from './components/Calendar';
 import { ClockCity } from './components/ClockCity';
-import localFont from '@next/font/local';
-const digital = localFont({ src: './fonts/digital.ttf' })
 
 export default function Home() {
   const now = new Date()
   return (
     <main>
-      <div className={`${digital.className}`}>
-        <Clock time={now.getTime()} />
-      </div>
+      <Clock time={now.getTime()} />
       <Calendar />
-      <div className={`${digital.className}`}>
-        <ClockCity /> 
-      </div>
+      <ClockCity /> 
     </main>  
   );
 }
