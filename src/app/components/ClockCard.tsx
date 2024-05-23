dc2554b49bd300e33991ec8b231e018d2fee8d28
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-05-23 10:55:08 -0300

    feat: changing clock font size

--- src/app/components/ClockCard.tsx
+++ src/app/components/ClockCard.tsx
@@ -36,16 +36,16 @@ export const ClockCard: React.FC<CityClockProps> = ({ cityName, timeZone, market
 
     return (
         <div className="w-64 h-48 text-white p-4 rounded-lg relative flex flex-col justify-center bg-white/5 z-10 backdrop-filter backdrop-blur-lg shadow-lg">
-            <div className="absolute top-4 left-4 text-sm font-semibold">
+            <div className="absolute top-3 left-4 text-lg font-semibold">
                 {cityName}
             </div>
-            <div className={`absolute top-4 right-4 text-sm font-semibold ${marketStatusColor}`}>
+            <div className={`absolute top-3 right-4 text-lg font-semibold ${marketStatusColor}`}>
                 {marketStatusText}
             </div>
             <hr className="w-full border-t border-[#1f1e1e]" />
             <div className="text-2xl font-bold text-center mt-4">
                 <p className="text-3xl font-bold tabular-nums">{time}</p>
-                <p className="text-sm mt-2">{date}</p>
+                <p className="text-custom mt-2">{date}</p>
             </div>
         </div>
     );
