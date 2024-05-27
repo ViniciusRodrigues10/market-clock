f1f8b042f52284e54c3359d2e39636ddbee6a2fa
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-05-27 17:38:35 -0300

    feat: add clock to website tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+'use client'
 import { Clock } from './components/Clock';
 import { Calendar } from './components/Calendar';
 import { ClockCity } from './components/ClockCity';
+import useClockTitle from '@/utils/useClockTitle';
 
 export default function Home() {
   const now = new Date()
+  useClockTitle();
+  
   return (
     <main className="flex min-h-screen flex-col bg-[#000000]">
       <Clock time={now.getTime()} />
